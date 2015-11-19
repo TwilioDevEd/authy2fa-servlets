d373e1d64706d6b8d375ad1a35ab4f5f1dc83209
Agustin Camino <agustin.camino@gmail.com>
2015-11-19 17:19:59 -0500

    Integrate OneTouch client with login

--- src/main/java/com/twilio/authy2fa/servlet/authentication/LogInServlet.java
+++ src/main/java/com/twilio/authy2fa/servlet/authentication/LogInServlet.java
@@ -1,12 +1,16 @@
 package com.twilio.authy2fa.servlet.authentication;
 
+import com.authy.onetouch.Client;
+import com.authy.onetouch.approvalrequest.Parameters;
 import com.twilio.authy2fa.lib.SessionManager;
 import com.twilio.authy2fa.models.User;
 import com.twilio.authy2fa.models.UserService;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.*;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @WebServlet(urlPatterns = {"/login"})
@@ -30,10 +34,21 @@ public class LogInServlet extends HttpServlet {
         User user = service.findByEmail(email);
         if (user != null && user.getPassword().equals(password)) {
             sessionManager.LogIn(request, user.getId());
+            sendApprovalRequest(user);
+
             response.sendRedirect("/welcome");
         } else {
             request.setAttribute("data", "Your credentials are incorrect");
             request.getRequestDispatcher("/login.jsp").forward(request, response);
         }
+    }
+
+    private void sendApprovalRequest(User user) throws IOException {
+        Client client = new Client(System.getenv("AUTHY_API_KEY"), user.getAuthyId());
+        Parameters parameters = Parameters.builder()
+                .addDetail("email", user.getEmail())
+                .build();
+
+        client.sendApprovalRequest("Request login to Twilio demo app", parameters);
     }
 }
